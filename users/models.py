e9daf7265a49aec8789a7d5b678da9ca41072d2b
guvzan <vernmaks@gmail.com>
2023-01-07 09:09:27 +0200

    Some Account Posts Correcrions

--- users/models.py
+++ users/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+def default_post_list():
+    return dict(id = [])
+
+def default_list_of_likers():
+    return dict(liked = [], saved = [])
+
 class CustomUser(AbstractUser):
     """Кастомний користувач"""
     status = models.CharField(max_length=100, default='---')
     dob = models.DateField(blank=True, null=True, default='1999-12-31')
     about = models.TextField(blank=True, null=True, default='---')
     image = models.ImageField(upload_to='images', blank=True, null=True)
-    post_list = models.JSONField(blank=True, null=True)
+    post_list = models.JSONField(blank=True, null=True, default=default_post_list)
 
 
 class PagePost(models.Model):
@@ -16,7 +22,7 @@ class PagePost(models.Model):
     image = models.ImageField(blank=True, null=True, upload_to='images/post_images')
     text = models.TextField(blank=True, null=True)
     likes = models.IntegerField(default=0)
-    list_of_likers = models.JSONField(blank=True, null=True)
+    list_of_likers = models.JSONField(blank=True, null=True, default=default_list_of_likers)
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, default=CustomUser.objects.get(id=1).id)
     date_added = models.DateTimeField(auto_now_add=True, blank=True, null=True)
 
--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
+
 
 from .forms import *
 from .models import *
@@ -26,7 +28,9 @@ def account(request, user_id):
     test_post = PagePost.objects.filter(id=1)
     if user.post_list:
         for id in user.post_list['id']:
-            user_posts.append(PagePost.objects.filter(id=id))
+            exact_post = PagePost.objects.filter(id=id)
+            if exact_post:
+                user_posts.insert(0, exact_post)
     context = {
         'accuser': user,
         'user_posts': user_posts
@@ -38,18 +42,24 @@ def add_acc_like(request, user_id, post_id):
     """Додати лайк"""
     post = PagePost.objects.get(id = post_id)
     post.like()
-    return redirect('users:account', user_id)
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER', 'films:index'))
 
 
 def share(request, post_id, user_id):
     """Поділитись дописом"""
     post = PagePost.objects.get(id = post_id)
     user = CustomUser.objects.get(id = user_id)
+    liked_list = post.list_of_likers['saved']
     saved_list = user.post_list['id']
+
+    if int(user_id) not in liked_list:
+        liked_list.append(user_id)
+        post.save()
+
     if int(post_id) not in saved_list:
         saved_list.append(post_id)
         user.save()
-    return redirect('films:index')
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER', 'films:index'))
 
 
 def create_page_post(request, user_id):
